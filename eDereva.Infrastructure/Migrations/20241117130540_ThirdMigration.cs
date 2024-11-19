7af2ee31b8747a3a39fd47c1d2fdd140d134908b
HKarata <herishine00@gmail.com>
2024-11-19 10:45:04 +0300

    Add ThirdMigration and ThirdUpdateMigration for VenueExemptions
    Implemented migrations to add the VenueExemptions table and modify the ContingencyId column in the Sessions table. Created necessary migration and designer files to reflect these updates. Additionally, added a new eDereva.Workflows project.

---
+++ eDereva.Infrastructure/Migrations/20241117130540_ThirdMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eDereva.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThirdMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VenueExemptions",
+                columns: table => new
+                {
+                    VenueId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExemptionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    HasBeenApproved = table.Column<bool>(type: "bit", nullable: false),
+                    HasBeenExempted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VenueExemptions", x => x.VenueId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VenueExemptions");
+        }
+    }
+}
