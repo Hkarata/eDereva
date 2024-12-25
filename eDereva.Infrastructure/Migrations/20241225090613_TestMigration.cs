307598caef07a4ef01f53d93058452dea845694a
HKarata <herishine00@gmail.com>
2024-12-25 12:06:59 +0300

    Refactor database schema to support tests and sections
    Revised the database structure to introduce test-related entities, including `Tests`, `SectionTemplates`, and `TestSections`. Removed `Choices` and `QuestionBanks`, renaming and reworking relevant relationships and columns (e.g., `QuestionBankId` to `SectionTemplateId`). Updated connection string in `appsettings.json` to use a local SQL Server instance.

---
+++ eDereva.Infrastructure/Migrations/20241225090613_TestMigration.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eDereva.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_QuestionBanks_QuestionBankId",
+                table: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "Choices");
+
+            migrationBuilder.DropTable(
+                name: "QuestionBanks");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Questions");
+
+            migrationBuilder.RenameColumn(
+                name: "QuestionBankId",
+                table: "Questions",
+                newName: "SectionTemplateId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Questions_QuestionBankId",
+                table: "Questions",
+                newName: "IX_Questions_SectionTemplateId");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TestSectionId",
+                table: "Questions",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "LicenseClasses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Class = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LicenseClasses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Options",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsCorrect = table.Column<bool>(type: "bit", nullable: false),
+                    QuestionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Options", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Options_Questions_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SectionTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SectionTemplates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    PassScore = table.Column<int>(type: "int", nullable: false),
+                    TestVersion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestSections",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TestId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SectionTemplateId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ContributionCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestSections", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TestSections_SectionTemplates_SectionTemplateId",
+                        column: x => x.SectionTemplateId,
+                        principalTable: "SectionTemplates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TestSections_Tests_TestId",
+                        column: x => x.TestId,
+                        principalTable: "Tests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_TestSectionId",
+                table: "Questions",
+                column: "TestSectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Options_QuestionId",
+                table: "Options",
+                column: "QuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestSections_SectionTemplateId",
+                table: "TestSections",
+                column: "SectionTemplateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestSections_TestId",
+                table: "TestSections",
+                column: "TestId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_SectionTemplates_SectionTemplateId",
+                table: "Questions",
+                column: "SectionTemplateId",
+                principalTable: "SectionTemplates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_TestSections_TestSectionId",
+                table: "Questions",
+                column: "TestSectionId",
+                principalTable: "TestSections",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_SectionTemplates_SectionTemplateId",
+                table: "Questions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_TestSections_TestSectionId",
+                table: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "LicenseClasses");
+
+            migrationBuilder.DropTable(
+                name: "Options");
+
+            migrationBuilder.DropTable(
+                name: "TestSections");
+
+            migrationBuilder.DropTable(
+                name: "SectionTemplates");
+
+            migrationBuilder.DropTable(
+                name: "Tests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_TestSectionId",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "TestSectionId",
+                table: "Questions");
+
+            migrationBuilder.RenameColumn(
+                name: "SectionTemplateId",
+                table: "Questions",
+                newName: "QuestionBankId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Questions_SectionTemplateId",
+                table: "Questions",
+                newName: "IX_Questions_QuestionBankId");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Questions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Questions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Choices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuestionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Choices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Choices_Questions_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuestionBanks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionBanks", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Choices_QuestionId",
+                table: "Choices",
+                column: "QuestionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_QuestionBanks_QuestionBankId",
+                table: "Questions",
+                column: "QuestionBankId",
+                principalTable: "QuestionBanks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
